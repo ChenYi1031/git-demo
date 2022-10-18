1a2fb3f89b08686fec502daeab1d0d427e29f347
陈翊 <chenyi01@greatwall.com.cn>
2022-10-18 11:03:20 +0800

    secend commit

--- src/main/java/GitDemoTest.java
+++ src/main/java/GitDemoTest.java
@@ -2,5 +2,6 @@ public class GitDemoTest {
     public static void main(String[] args) {
         System.out.println("first commit");
         System.out.println("secend commit");
+        System.out.println("third commit");
     }
 }
